409777f84a11416430ba9850f2f6a8973e7f058f
saurabh.gupta4 <saurabh.gupta4@globallogic.com>
2019-01-18 09:41:51 +0530

    completed

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Json;
+using ChangeResponseType.Models;
+
+namespace ChangeResponseType.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        //returns xml if in header Accept : application/xml is passed
+        // GET api/values/json
+        [HttpGet("json")]
+        public Message GetParam(string msg)
+        {
+            Message message = new Message{ Value = msg };
+            return message;
+        }
+
+        //returns always xml
+        // GET api/values/xml
+        [HttpGet("xml")]
+        [Produces("application/xml")]
+        public Message GetXML(string msg)
+        {
+            Message message = new Message{ Value = msg };
+            return message;
+        }
+    }
+}
---
+++ Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChangeResponseType.Models
+{
+    //Model for response
+    public class Message
+    {
+        public string Value { get; set; }
+    }
+}
